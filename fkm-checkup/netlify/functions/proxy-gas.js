db83a773bb2112dde4d92f587cbe874c0e9cef22
mana-01 <95159204+mana-01@users.noreply.github.com>
2025-10-28 17:19:55 +0900

    Update proxy-gas.js

--- fkm-checkup/netlify/functions/proxy-gas.js
+++ fkm-checkup/netlify/functions/proxy-gas.js
@@ -6,7 +6,7 @@
  */
 
 exports.handler = async (event) => {
-  const GAS_URL = 'https://script.google.com/macros/s/AKfycbwar-e9SYeG3ID_Bof3vzbuXfewUb5Nxg7ufx6EFTXKVATlLmCN5ZRZujkdjpZlI8K5/exec';
+  const GAS_URL = 'https://script.google.com/macros/s/AKfycbz8dbmVpMrDfb4ortWuzNyFfBmom9LkEAcFdf14qhJ_DyXV-Y1W0_4piWtx4H7xjs2U/exec';
 
   // Allow both GET (data fetch) and POST (data save) through this single endpoint
   if (event.httpMethod === 'GET') {
